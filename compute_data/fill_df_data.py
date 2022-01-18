139c05c71177f1334dfe9e186c0dd99bfd156874
Extracheesy <desportes.cedric@gmail.com>
2022-01-18 15:12:06 +0100

    ___

--- compute_data/fill_df_data.py
+++ compute_data/fill_df_data.py
@@ -44,7 +44,7 @@ def fill_df(input_file):
 
     print("symbols with data: ", len(list_stock_no_data), " => ", filename)
     print("Symbols with ISNI: ", len(df_isni), " => ", filename_isni)
-    print("Dropped symbols:   ", len(list_stock_dropped), " => Get the fuck out")
+    print("Dropped symbols:   ", len(list_stock_dropped), " =>  Get the fuck out")
     # print(list_stock_no_data)
 
     df.to_csv(filename)
